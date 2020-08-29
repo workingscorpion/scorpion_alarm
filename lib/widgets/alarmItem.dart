2237bb14b91f6e3e8c8e37fb9ae41066c4d66b6c
workingscorpion <workingscorpion@jinjoosoft.com>
2020-08-30 07:54:20 +0900

    feat: alarmItem widget

--- lib/widgets/alarmItem.dart
+++ lib/widgets/alarmItem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scorpion_alarm_app/model/alarmModel.dart';
+import 'package:intl/intl.Dart';
 
 class AlarmItem extends StatelessWidget {
   AlarmItem(this.alarm);
@@ -9,8 +10,8 @@ class AlarmItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Image.asset(''),
-      title: Text(''),
+      leading: Image.asset('assets/icons/chicken.png'),
+      title: Text('${DateFormat("kk:mm").format(alarm.alarmTime).toString()}'),
     );
   }
 }
